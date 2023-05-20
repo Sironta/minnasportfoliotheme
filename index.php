cb16d07a4b9ed5304ec0bfceb0df8b5c7fd24227
Sironta <123162394+Sironta@users.noreply.github.com>
2023-05-20 17:56:00 +0300

    Added files in Repository

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minna's Portfolio</title>
+    <link rel="stylesheet" href="<?php bloginfo("stylesheet_url") ?>">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <h1>This is my home page!</h1>
+    <button class="primary-btn">
+        Press me
+    </button>
+    <button class="secondary-btn">
+        Or me!
+    </button>
+    <p>Nothing happens if you press tho!</p>
+    <p class="tiny-p">Haa-haa!</p>
+</body>
+</html>
