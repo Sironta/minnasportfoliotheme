c3cb2aaa4cfed0dae773f863a50bdd78446da092
Sironta <123162394+Sironta@users.noreply.github.com>
2023-05-23 11:35:14 +0300

    Created a breakpoint and grid system and fixed some minor stuff

--- index.php
+++ index.php
@@ -8,37 +8,66 @@
     <link rel="stylesheet" href="<?php bloginfo("stylesheet_url") ?>">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-    </nav>
-    <h1>h1 Heading</h1>
-    <h2>h2 Heading</h2>
-    <h3>h3 Heading</h3>
-    <h4>h4 Heading</h4>
-    <h5>h5 Heading</h5>
-    <h6>h6 Heading</h6>
-    <p>Paragrapgh</p>
-    <p class="tiny-p">Tiny paragraph</p>
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
 
+        <h1>h1 Heading</h1>
+        <h2>h2 Heading</h2>
+        <h3>h3 Heading</h3>
+        <h4>h4 Heading</h4>
+        <h5>h5 Heading</h5>
+        <h6>h6 Heading</h6>
+        <p>Paragrapgh</p>
+        <p class="tiny">Tiny paragraph</p>
 
-    <button class="primary-btn">
-        Primary button
-    </button>
-    <button class="secondary-btn">
-        Secondary button
-    </button>
-    <button class="tertiary-btn">
-        Tertiary button
-    </button>
+        <button class="primary-btn">
+            Primary button
+        </button>
+        <button class="secondary-btn">
+            Secondary button
+        </button>
+        <button class="tertiary-btn">
+            Tertiary button
+        </button>
+
+        <div class="row gap-2 justify-center">
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum dolor.</h3>
+                <div class="card-test">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </div>
+            </div>
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum dolor.</h3>
+                <div class="card-test">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </div>
+            </div>
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum dolor.</h3>
+                <div class="card-test">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </div>
+            </div>
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum dolor.</h3>
+                <div class="card-test">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
