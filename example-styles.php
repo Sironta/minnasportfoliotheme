7f7755416d9d6be8d4c56f023c82f84a1c5cfcd8
Sironta <123162394+Sironta@users.noreply.github.com>
2023-05-24 16:54:16 +0300

    Added php files for header, index and footer

---
+++ example-styles.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minna's Portfolio</title>
+    <link rel="stylesheet" href="<?php bloginfo("stylesheet_url") ?>">
+</head>
+<body>
+    <header class="container">
+        <div class="header container">
+            <h2 class="Site title">Portfolio</h2>
+            <nav class="primary-nav">
+                <ul>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <p>Cheat sheet for styling.</p>
+        </div>
+    </header>
+    <div class="container">
+        <h1>h1 Heading</h1>
+        <h2>h2 Heading</h2>
+        <h3>h3 Heading</h3>
+        <h4>h4 Heading</h4>
+        <h5>h5 Heading</h5>
+        <h6>h6 Heading</h6>
+        <p>Paragrapgh with <a href="#link">a link</a> and <a href="#another">another</a></p>
+        <p class="tiny">Tiny paragraph</p>
+
+        <button class="primary-btn">
+            Primary button
+        </button>
+        <button class="secondary-btn">
+            Secondary button
+        </button>
+        <button class="tertiary-btn">
+            Tertiary button
+        </button>
+
+        <div class="row gap-2 justify-center">
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum.</h3>
+                <div class="card-test">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum.</h3>
+                <div class="card-test">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum.</h3>
+                <div class="card-test">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="col-12-xs col-5-sm col-3-xl">
+                <h3>Lorem, ipsum.</h3>
+                <div class="card-test">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minna's Portfolio</title>
+    <link rel="stylesheet" href="<?php bloginfo("stylesheet_url") ?>">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header class="container">
+        <div class="header container">
+            <h2 class="Site title">Portfolio</h2>
+            <nav class="primary-nav">
+                <ul>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <p>Cheat sheet for styling.</p>
+        </div>
+    </header>
