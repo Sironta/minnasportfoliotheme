2be2df0510e4915e7fe1c6e38f26b0c0238d3db7
Sironta <123162394+Sironta@users.noreply.github.com>
2023-06-01 13:01:22 +0300

    Modified the semantics of HTML, mostly in WordPress

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <div class="container">
+    <main class="container">
 
     <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
@@ -12,6 +12,6 @@
 
         <?php endif; ?>
 
-    </div>
+    </main>
 
 <?php get_footer(); ?>
