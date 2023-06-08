ffcdd560bfdcdd8d1809666e3253795d622048f5
Sironta <123162394+Sironta@users.noreply.github.com>
2023-06-08 16:23:20 +0300

    Styling portfolio page

--- page-portfolio.php
+++ page-portfolio.php
@@ -8,13 +8,17 @@
 
     <?php $categories = get_categories(); ?>
 
+    <div class="category-headings align-self-center">
     <?php if ($categories) {
     foreach ($categories as $category) {
+        echo '<div class="category-heading align-self-center">';
         echo '<a href="' . get_category_link($category->term_id) . '">';
         echo '<h2>' . $category->name . '</h2>';
         echo '</a>';
+        echo '</div>';
     }
     } ?>
+    </div>
 
         <?php endwhile; else: ?>
 
