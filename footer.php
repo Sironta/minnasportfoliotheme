52807a3256fcf17b57b5700b28ffe4a2492c76cb
Sironta <123162394+Sironta@users.noreply.github.com>
2023-07-12 17:31:34 +0300

    Changed header color change transition time

--- footer.php
+++ footer.php
@@ -52,10 +52,10 @@
     window.addEventListener('scroll', function() {
     let headerContainer = document.getElementById('headerContainer');
     if (window.scrollY > 0) {
-            headerContainer.style.transition = 'background-color 1s';
+            headerContainer.style.transition = 'background-color 0.5s';
             headerContainer.style.backgroundColor = 'var(--secondary-color)';
         } else {
-            headerContainer.style.transition = 'background-color 1s';
+            headerContainer.style.transition = 'background-color 0.5s';
             headerContainer.style.backgroundColor = ''; // Reset to default color
         }
     });
