c4f347cfcc7260461492be507aeac41393553d6b
Sironta <123162394+Sironta@users.noreply.github.com>
2023-09-13 12:35:18 +0300

    Purged style.css file in cmd

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    
-}
+    content: [
+        './src/content-page.php',
+        './src/footer.php',
+        './src/functions.php',
+        './src/header.php',
+        './src/index.php',
+        './src/page-portfolio.php',
+        './src/single.php',
+        './src/category.php'
+    ],
+    css: ['./style.css'],
+};
